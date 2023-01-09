e6743bcc6d0664dfbc29dd722887d8ef39122553
Manish Mazumdar <bboyacademician@gmail.com>
2023-01-09 11:42:41 +0530

    Second Commit

--- Movies/routes/movies_routes.js
+++ Movies/routes/movies_routes.js
@@ -22,18 +22,7 @@ router.route("/api/v1/new-movie").post((req,res)=>{
 })
 
 
-function storeNextIndexNextIndex(){
-    sqlDB.query("SELECT * FROM movies ORDER BY tconst DESC LIMIT 1", function (err, result, fields) { 
-     nextIndex = incrementIndex(result[0].tconst)
-    });
-}
 
-
-function incrementIndex(value){
-let nextNumber = "tt" + ("00000" + (parseInt(value.slice(2)) + 1)).slice(-5);
-return nextNumber
-
-}
 
 
 
